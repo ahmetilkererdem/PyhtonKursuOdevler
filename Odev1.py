05e1f75770ae56f3b0fb9aa7711691bc0ee9e449
Ahmet İlker Erdem <105577943+ahmetilkererdem@users.noreply.github.com>
2022-05-15 09:37:49 +0300

    Create Odev1.py

---
+++ Odev1.py
@@ -0,0 +1,8 @@
+# ÖDEV
+# SORU Klavyeden girilen 2 sayıdan 1. nin büyük olup olmadığını ekrana yazdıran kod..
+
+birinci_sayi = input("Lütfen Birinci Sayıyı Giriniz: ")
+ikinci_sayi = input("Lütfen İkinci Sayıyı Giriniz: ")
+
+birinci_sayi_buyuk_mu = birinci_sayi > ikinci_sayi
+print(birinci_sayi_buyuk_mu)
